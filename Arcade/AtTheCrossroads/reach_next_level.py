e455c7e8f07dcab1d9c25266ca54b933dfd0ed15
Katie Byers <lobsterkatie@gmail.com>
2016-11-14 04:17:28 -0800

    added reach_next_level

---
+++ Arcade/AtTheCrossroads/reach_next_level.py
@@ -0,0 +1,43 @@
+"""
+AT THE CROSSROADS / REACH NEXT LEVEL
+
+You are playing an RPG game. Currently your experience points (XP) total is
+equal to experience. To reach the next level your XP should be at least at
+threshold. If you kill the monster in front of you, you will gain more
+experience points in the amount of the reward.
+
+Given values experience, threshold and reward, check if you reach the next
+level after killing the monster.
+
+Example
+
+    For experience = 10, threshold = 15 and reward = 5, the output should be
+    reachNextLevel(experience, threshold, reward) = true;
+    For experience = 10, threshold = 15 and reward = 4, the output should be
+    reachNextLevel(experience, threshold, reward) = false.
+
+Input/Output
+
+    [input] integer experience
+
+        Constraints:
+        3 ≤ experience ≤ 250.
+
+    [input] integer threshold
+
+        Constraints:
+        5 ≤ threshold ≤ 300.
+
+    [input] integer reward
+
+        Constraints:
+        2 ≤ reward ≤ 65.
+
+    [output] boolean
+
+        true if you reach the next level, false otherwise.
+"""
+
+
+def reachNextLevel(experience, threshold, reward):
+    return experience + reward >= threshold
