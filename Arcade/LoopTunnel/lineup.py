5cce444b522ae1fdbe2ec5c9b40f2531cfec0ec6
Katie Byers <lobsterkatie@gmail.com>
2016-11-17 21:22:00 -0800

    added linup

---
+++ Arcade/LoopTunnel/lineup.py
@@ -0,0 +1,55 @@
+"""
+LOOP TUNNEL / LINEUP
+
+To prepare his students for an upcoming game, the sports coach decides to try
+some new training drills. To begin with, he lines them up and starts with the
+following warm-up exercise: when the coach says 'L', he instructs the students
+to turn to the left. Alternatively, when he says 'R', they should turn to the
+right. Finally, when the coach says 'A', the students should turn around.
+
+Unfortunately some students (not all of them, but at least one) can't tell left
+from right, meaning they always turn right when they hear 'L' and left when
+they hear 'R'. The coach wants to know how many times the students end up
+facing the same direction.
+
+Given the list of commands the coach has given, count the number of such
+commands after which the students will be facing the same direction.
+
+Example
+
+    For commands = "LLARL", the output should be
+    lineUp(commands) = 3.
+
+    Let's say that there are 4 students, and the second one can't tell left
+    from right. In this case, only after the second, third and fifth commands
+    will the students face the same direction.
+
+    [graphic removed]
+
+Input/Output
+
+    [input] string commands
+
+        String consisting of characters 'L', 'R' and 'A' only.
+
+        Constraints:
+        0 ≤ commands.length ≤ 35.
+
+    [output] integer
+
+        The number of commands after which students face the same direction.
+"""
+
+
+def lineUp(commands):
+    count = 0
+    facing_the_same_way = True
+    for command in commands:
+        if ((facing_the_same_way and command in ["L", "R"]) or
+            (not facing_the_same_way and command == "A")):
+            facing_the_same_way = False
+        else:
+            facing_the_same_way = True
+            count += 1
+
+    return count
