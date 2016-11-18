61484471c09b9a1600473d5f93e50ab504fb8862
Katie Byers <lobsterkatie@gmail.com>
2016-11-17 21:48:40 -0800

    added create_array

---
+++ Arcade/ListForestEdge/create_array.py
@@ -0,0 +1,25 @@
+"""
+LIST FOREST EDGE / CREATE ARRAY
+
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+    For size = 4, the output should be
+    createArray(size) = [1, 1, 1, 1].
+
+Input/Output
+
+    [input] integer size
+
+        A positive integer.
+
+        Constraints:
+        1 ≤ size ≤ 10.
+
+    [output] array.integer
+"""
+
+
+def createArray(size):
+    return [1] * size
